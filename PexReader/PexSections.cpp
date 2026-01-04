d625e213c62c860a916c1de588267ab2027134ad
YD525 <525084464@qq.com>
2026-01-04 15:16:14 +0800

    update

--- PexReader/PexSections.cpp
+++ PexReader/PexSections.cpp
@@ -1,6 +1,19 @@
 #pragma once
 #include "Record.h"
 
+// Forward Declarations
+struct DebugFunction;
+struct Variable;
+struct Property;
+struct State;
+struct Instruction;
+struct Function;
+struct UserFlag;
+struct Parameter;
+struct LocalVariable;
+
+
+#pragma region String Table 
 //String Table 
 struct StringTable
 {
@@ -14,7 +27,25 @@ struct StringTable
         strings.resize(count);
     }
 };
+#pragma endregion
+#pragma region DebugInfo 
+//Debug Info 
+struct DebugInfo
+{
+    uint8_t hasDebugInfo;//Flag, if zero then no debug info is present and the rest of the record is skipped
+    uint64_t modificationTime; //time_t
+    uint16_t functionCount;
+    vector<DebugFunction> functions;
 
+    DebugInfo() :hasDebugInfo(0), modificationTime(0), functionCount(0) {}
+
+    DebugInfo(uint16_t _count) :hasDebugInfo(0), modificationTime(0), functionCount(_count)
+    {
+        functions.resize(functionCount);
+    }
+};
+#pragma endregion
+#pragma region DebugFunction
 //Debug Function
 struct DebugFunction
 {
@@ -33,53 +64,95 @@ struct DebugFunction
         : objectNameIndex(0), stateNameIndex(0), functionNameIndex(0),
         functionType(0), instructionCount(_instructionCount)
     {
-        lineNumbers.resize(instructionCount); 
+        lineNumbers.resize(instructionCount);
     }
 };
-
-//Debug Info 
-struct DebugInfo
-{
-    uint8_t hasDebugInfo;//Flag, if zero then no debug info is present and the rest of the record is skipped
-    uint64_t modificationTime;   
-    uint16_t functionCount;      
-    vector<DebugFunction> functions;
-
-    DebugInfo() :hasDebugInfo(0), modificationTime(0), functionCount(0) {}
-
-    DebugInfo(uint16_t _count) :hasDebugInfo(0), modificationTime(0), functionCount(_count)
-    {
-        functions.resize(functionCount);
-    }
-};
-
+#pragma endregion
+#pragma region User Flag
+//UserFlag
 #pragma pack(push, 1)
 struct UserFlag
 {
-    uint16_t flagNameIndex;  
-    uint8_t flagIndex;       
+    uint16_t flagNameIndex;
+    uint8_t flagIndex;
 };
 #pragma pack(pop)
-
-#pragma pack(push, 1)
-struct VariableType
+#pragma endregion
+#pragma region Object&Object Data
+//Object Object Data
+struct Object
 {
-    uint16_t typeNameIndex;  
+    uint16_t nameIndex;
+    uint32_t size;
+
+    ObjectData data; 
+
+    Object(uint16_t _nameIndex, uint32_t _size)
+        : nameIndex(_nameIndex), size(_size)
+    {
+    }
 };
-#pragma pack(pop)
+struct ObjectData
+{
+    uint16_t parentClassName;
+    uint16_t docString;
+    uint32_t userFlags;
+    uint16_t autoStateName;
+    uint16_t numVariables;
+    std::vector<Variable> variables;
+    uint16_t numProperties;
+    std::vector<Property> properties;
+    uint16_t numStates;
+    std::vector<State> states;
+};
 
 #pragma pack(push, 1)
+struct Variable
+{
+    uint16_t name;
+    uint16_t typeName;
+    uint32_t userFlags;
+    VariableData data;
+};
 struct VariableData
 {
-    uint16_t nameIndex;     
-    uint16_t typeNameIndex;  
-    uint32_t userFlags;      
-    uint32_t data;           
+    uint8_t type;  // Type: 0 = null, 1 = identifier, 2 = string, 3 = integer, 4 = float, 5 = bool
+    std::variant<uint16_t, int32_t, float, uint8_t, std::string> data;  // Data of different types
+
+    VariableData(uint8_t _type) : type(_type)
+    {
+        if (type == 0 || type == 1 || type == 2) {
+            data = uint16_t(0);  //uint16_t
+        }
+        else if (type == 3) {
+            data = int32_t(0);  //int32_t
+        }
+        else if (type == 4) {
+            data = float(0.0f);  //float
+        }
+        else if (type == 5) {
+            data = uint8_t(0);  //bool (uint8_t)
+        }
+    }
+
+    VariableData(uint8_t _type, const std::string& str) : type(_type)
+    {
+        if (type == 2) {
+            data = str;
+        }
+        else {
+            *this = VariableData(_type);
+        }
+    }
 };
 #pragma pack(pop)
 
+#pragma endregion
+
+
+
 #pragma pack(push, 1)
-struct PropertyData
+struct Property
 {
     uint16_t nameIndex;      
     uint8_t type;            
@@ -135,31 +208,7 @@ struct Function
     vector<Instruction> instructions;
 };
 
-struct Object
-{
-    uint16_t nameIndex;          
-    uint16_t parentNameIndex;    
-    uint16_t ownerNameIndex;     
-    uint16_t docStringIndex;     
-    uint32_t userFlags;          
-    uint16_t autoStateNameIndex; 
 
-
-    uint16_t variableCount;      
-    vector<VariableData> variables;
-
-
-    uint16_t propertyCount;      
-    vector<PropertyData> properties;
-
-
-    uint16_t stateCount;         
-    vector<State> states;
-
-
-    uint16_t functionCount;      
-    vector<Function> functions;
-};
 
 struct RecordSections
 {
